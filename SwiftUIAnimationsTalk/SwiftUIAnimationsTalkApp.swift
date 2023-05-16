af8fe081df8edd69be8fd63f1532ac8b4197e2a9
Michael Freiwald <michael@freiwald.dev>
2023-05-16 20:00:27 +0200

    Initial Commit

---
+++ SwiftUIAnimationsTalk/SwiftUIAnimationsTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsTalkApp.swift
+//  SwiftUIAnimationsTalk
+//
+//  Created by Michael on 16.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
